277bb865306cd7f4c38931d3543e889894ba954f
Jake Allen <jake.allen@me.com>
2021-02-01 20:50:15 -0500

    add randomize function

--- message-generator.js
+++ message-generator.js
@@ -2,3 +2,7 @@ const name = ['Chief', 'Ace', 'Bud', "Guv'na", 'Champ', 'Honorable Sir'];
 const noun = ['brewski', 'tennis racket', 'kayak', 'car keys'];
 const activity = ['drinking', 'fishing', 'hiking', 'for a drive', 'on an adventure'];
 
+
+function randomize(num){
+    return Math.floor(Math.random() * num);
+};
