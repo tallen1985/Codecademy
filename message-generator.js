b78a1f21c61d12e299d33c03765de90c74994881
Jake Allen <jake.allen@me.com>
2021-02-02 05:58:44 -0500

    add comments

--- message-generator.js
+++ message-generator.js
@@ -1,11 +1,14 @@
+//create arrays to be populated with messages
 var nickname = ['Chief', 'Ace', 'Bud', "Guv'na", 'Champ', 'Honorable Sir'];
 var noun = ['brewski', 'tennis racket', 'kayak', 'car keys'];
 var activity = ['drinking', 'fishing', 'hiking', 'for a drive', 'on an adventure'];
 
+//randomization
 function randomize(num){
     return Math.floor(Math.random() * num);
 };
 
+//function to actually make the message
 function createMessage(name, noun, activity){
     let message = "Hey ";
     message += name[randomize(name.length)] + ", grab your ";
@@ -14,4 +17,5 @@ function createMessage(name, noun, activity){
     return message;
 }
 
+//call function
 console.log(createMessage(nickname,noun, activity));
