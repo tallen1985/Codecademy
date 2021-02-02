146d4cb3bfa40cc2d668e186c034dc9bbb1a510b
Jake Allen <jake.allen@me.com>
2021-02-01 21:00:44 -0500

    add createMessage()

--- message-generator.js
+++ message-generator.js
@@ -1,8 +1,17 @@
-const name = ['Chief', 'Ace', 'Bud', "Guv'na", 'Champ', 'Honorable Sir'];
-const noun = ['brewski', 'tennis racket', 'kayak', 'car keys'];
-const activity = ['drinking', 'fishing', 'hiking', 'for a drive', 'on an adventure'];
-
+var nickname = ['Chief', 'Ace', 'Bud', "Guv'na", 'Champ', 'Honorable Sir'];
+var noun = ['brewski', 'tennis racket', 'kayak', 'car keys'];
+var activity = ['drinking', 'fishing', 'hiking', 'for a drive', 'on an adventure'];
 
 function randomize(num){
     return Math.floor(Math.random() * num);
 };
+
+function createMessage(name, noun, activity){
+    let message = "Hey ";
+    message += name[randomize(name.length)] + ", grab your ";
+    message += noun[randomize(noun.length)] + ", we're going ";
+    message += activity[randomize(activity.length)] + ".";
+    return message;
+}
+
+console.log(createMessage(nickname,noun, activity));
