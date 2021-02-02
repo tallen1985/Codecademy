6bf7ee624973721b002b4f5699422450eb5f9f5f
Jake Allen <jake.allen@me.com>
2021-02-01 20:49:33 -0500

    construct arrays for message

---
+++ message-generator.js
@@ -0,0 +1,4 @@
+const name = ['Chief', 'Ace', 'Bud', "Guv'na", 'Champ', 'Honorable Sir'];
+const noun = ['brewski', 'tennis racket', 'kayak', 'car keys'];
+const activity = ['drinking', 'fishing', 'hiking', 'for a drive', 'on an adventure'];
+
