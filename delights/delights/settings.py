dc65a27d0bc0dea522695d5006ae1677553aa58d
Troy Allen <jake.allen@me.com>
2021-05-25 14:50:46 -0400

    up and running

--- delights/delights/settings.py
+++ delights/delights/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z7o=$i8!8d2hh+(%p9&p)f7u1affg4s*)4fxkri#cm(d#awn5a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tallen1985.pythonanywhere.com', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_delights.apps.DjangoDelightsConfig'
+
 ]
 
 MIDDLEWARE = [
